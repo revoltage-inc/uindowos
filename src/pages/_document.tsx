823361cd7447c1c3bc402030ae793970377eb762
yogarasu <ishiguro@revoltage.jp>
2022-10-23 01:35:20 +0900

    fix #60 add document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,17 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta name="description" content="UindowOS" />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="stylesheet" href="https://use.typekit.net/meh2xys.css" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -7,9 +7,6 @@ const LoginPage: NextPage = () => {
     <>
       <Head>
         <title>UindowOS | Login</title>
-        <meta name="description" content="UindowOS" />
-        <link rel="icon" href="/favicon.png" />
-        <link rel="stylesheet" href="https://use.typekit.net/meh2xys.css" />
       </Head>
       <main>
         <Login />
